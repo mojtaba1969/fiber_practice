5dfb43416905181d0eb0d1be0f6d5b226232fd50
Dire Titan <parsa.esfahanian.7596@gmail.com>
2021-07-15 20:11:33 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+go mod init test
+go mod tidy
+go get -u github.com/gofiber/fiber/v2
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New(fiber.Config{
+		CaseSensitive: true,
+		StrictRouting: false,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/test", func(c *fiber.Ctx) error {
+		return c.SendString("This is a test!")
+	})
+
+	app.Listen(":3000")
+}
